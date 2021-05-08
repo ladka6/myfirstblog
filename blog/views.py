6062806649eb0653533b2fb35c31f2507c91da9f
ladka6 <ege-erdal101@hotmail.com>
2021-05-08 13:10:42 +0300

    Veritabanı güncellemesi

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models improt Post
+from django.utils improt timezone
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+
+
